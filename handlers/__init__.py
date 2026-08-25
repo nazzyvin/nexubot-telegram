aef781b71af7c2af7ed91a175c4317ae3ced2a64
Naz-k <nazkvincent@gmail.com>
2026-08-25 10:57:00 +0100

    Add get_activity_tracker_handler to handlers - 25/08/2026

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,7 +2,7 @@ from .basic import start, help_command, ping
 from .fun import eightball, coinflip, roll, choose, rps
 from .stickers import convert_command
 from .vault import save_command, get_command, mylist_command, del_command
-from .games import get_game_handlers
+from .games import get_game_handlers, get_activity_tracker_handler
 from .group import get_group_handlers
 
 __all__ = [
@@ -10,5 +10,5 @@ __all__ = [
     'eightball', 'coinflip', 'roll', 'choose', 'rps',
     'convert_command',
     'save_command', 'get_command', 'mylist_command', 'del_command',
-    'get_game_handlers', 'get_group_handlers',
+    'get_game_handlers', 'get_activity_tracker_handler', 'get_group_handlers',
 ]
